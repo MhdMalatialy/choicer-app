f16daf0db9fbc298783b317416e4ff5e3244c9e9
Mohammad Malatialy <mohammad.maltialy@gmail.com>
2021-03-31 00:03:43 +0300

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log('App.js is running!'); // JSX - JavaScript XML
+
+var template = /*#__PURE__*/React.createElement("h1", null, "The-Choicer");
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
